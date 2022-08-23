8b382a01a93395ef5cd277ae4cf7d1d936cc7271
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-23 20:01:31 +0000

    Restart the program until we say 'no'

--- main.py
+++ main.py
@@ -3,45 +3,49 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 import art
 
 print(art.logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def encrypt(text,shift):
-  result = ''
-  for char in text:
-    if char in alphabet:
-      index = alphabet.index(char)
-      shifted = index + shift
-      if shifted > len(alphabet)-1:
-        difference = shifted - len(alphabet)
-        result+= alphabet[difference]
+restart = 'yes'
+while restart == 'yes':
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  def encrypt(text,shift):
+    result = ''
+    for char in text:
+      if char in alphabet:
+        index = alphabet.index(char)
+        shifted = index + shift
+        if shifted > len(alphabet)-1:
+          difference = shifted - len(alphabet)
+          result+= alphabet[difference]
+        else:
+          result += alphabet[shifted]
       else:
-        result += alphabet[shifted]
-    else:
-      result+=char
-
-  print(result)
-
-def decrypt(text,shift):
-  result = ''
-  for char in text:
-    if char in alphabet:
-      index = alphabet.index(char)
-      shifted = index - shift
-      if shifted < 0:
-        difference = len(alphabet) + shifted
-        result += alphabet[difference] 
+        result+=char
+  
+    print(result)
+  
+  def decrypt(text,shift):
+    result = ''
+    for char in text:
+      if char in alphabet:
+        index = alphabet.index(char)
+        shifted = index - shift
+        if shifted < 0:
+          difference = len(alphabet) + shifted
+          result += alphabet[difference] 
+        else:
+          result += alphabet[shifted]
       else:
-        result += alphabet[shifted]
-    else:
-      result += char
+        result += char
+  
+    print(result)
+  
+  if(direction == 'encode'):
+    encrypt(text,shift)
+  elif(direction == 'decode'):
+    decrypt(text,shift)
+  else:
+    print("INVALID REQUEST")
 
-  print(result)
-
-if(direction == 'encode'):
-  encrypt(text,shift)
-elif(direction == 'decode'):
-  decrypt(text,shift)
-else:
-  print("INVALID REQUEST")
+  restart = input("Do you want to restart? 'yes' or 'no' >> ")
